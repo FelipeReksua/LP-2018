799c6a1310068f8ec90e944b051c1e40a4597d48
JKovalhuk <kovalhukjoaovitor@gmail.com>
2018-05-10 12:07:10 -0300

    Final
    Utilização da palavra reservada "final"

---
+++ atividade-20/Principal.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author joaov
+ */
+public class Principal {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        final int x;
+        
+
+        // TODO code application logic here
+        x = 4;
+        //x++;
+        //Após alterar x para final o programa não executa. Depois de stribuído 4 à x o valor não pode mais ser alterado
+        System.out.printf("%d\n",x);
+        
+        
+    }
+
+}
